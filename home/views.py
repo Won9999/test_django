7ad2c637f8443b5fb7beac6e45e34ef42acbd678
Won9999 <github.com/Won9999>
2021-07-09 17:57:23 +0900

    Httresponse

--- home/views.py
+++ home/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse('<h1>안녕하세요 누구누구입니다.</h1>')
+    path= request.path
+    resultstr = ''
+    if path == '/home':
+        resultstr = '<h1>Hi home</h1>'
+    else :
+        resultstr = '<h1>here are main</h1>'
+    return HttpResponse(resultstr)
+
+def index01(request):
+    result = {'first':'Sanghun','second':'Oh'}
+    return render(request, 'index.html', context=result)
+
+def index02(request):
+    result = {'first':request.GET['first'],'second':request.GET['second']}
+    return render(request, 'index.html', context=result)
--- web_config/urls.py
+++ web_config/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from urllib import request
 
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
 
 from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', views.index)
+    path('home', views.index),
+    path('',views.index),
+    path('index01', views.index01),
+    path('index02', views.index02),
+
 ]
